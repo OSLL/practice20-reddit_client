86ccbc5ffada27097f946ab66c7a8ac54e480c08
fedor <fedorblokhin@mail.ru>
2020-06-05 00:29:57 +0300

    подключил сеттингсы

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/fragments/AccountFragment.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/fragments/AccountFragment.kt
@@ -1,11 +1,15 @@
 package com.makentoshe.androidgithubcitemplate.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.xml.R
+import com.makentoshe.androidgithubcitemplate.SettingsActivity
+import kotlinx.android.synthetic.main.profile_layout.*
 
 class AccountFragment : Fragment {
     constructor() : super()
@@ -16,6 +20,10 @@ class AccountFragment : Fragment {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.profile_layout, container, false)
+        (view.findViewById<ImageView>(R.id.settingsButton)).setOnClickListener {
+            val intent = Intent(activity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
         return view
     }
 }
